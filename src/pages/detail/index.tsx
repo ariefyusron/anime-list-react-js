375931f249069e2731a867e16ba3e11c40aec372
Arief Yusron <ariefyusron98@gmail.com>
2025-03-10 11:11:27 +0700

    feat(detail): page detail

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,15 +1,113 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 import { useGetAnimeDetail } from "../../hooks/useAnime";
+import Loader from "../../components/loader";
+
+import {
+  Topbar,
+  Title,
+  ButtonBack,
+  Content,
+  Image,
+  WrapImage,
+  Text
+} from "./styles";
 
 const Detail = () => {
   const { malId } = useParams<{ malId: string }>();
+  const navigate = useNavigate();
+
   const { data, isLoading } = useGetAnimeDetail(String(malId));
 
-  console.log(data);
-  console.log(isLoading);
+  const informationData = {
+    Type: data?.data.type,
+    Episodes: data?.data.episodes,
+    Status: data?.data.status,
+    Aired: data?.data.aired.string,
+    Broadcast: data?.data.broadcast.string,
+    Producers: data?.data.producers.map((item) => item.name).join(", "),
+    Licensors: data?.data.licensors.map((item) => item.name).join(", "),
+    Studios: data?.data.studios.map((item) => item.name).join(", "),
+    Source: data?.data.source,
+    Genres: data?.data.genres.map((item) => item.name).join(", "),
+    Themes: data?.data.themes.map((item) => item.name).join(", "),
+    Demographics: data?.data.demographics.map((item) => item.name).join(", "),
+    Duration: data?.data.duration,
+    Rating: data?.data.rating
+  };
 
-  return <div>Detail</div>;
+  const statisticData = {
+    Score: data?.data.score,
+    Ranked: data?.data.rank,
+    Popularity: data?.data.popularity,
+    Members: data?.data.members,
+    Favorites: data?.data.favorites
+  };
+
+  return (
+    <>
+      <Topbar>
+        <ButtonBack onClick={() => navigate(-1)}>back</ButtonBack>
+        <Title>{isLoading ? "Loading..." : data?.data.title}</Title>
+      </Topbar>
+
+      {isLoading ? (
+        <Loader
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "100px"
+          }}
+        />
+      ) : (
+        <Content>
+          <WrapImage>
+            <Image
+              alt="image-detail"
+              src={data?.data.images.webp.large_image_url}
+            />
+
+            <Text>{data?.data.title_japanese}</Text>
+            <Text style={{ marginTop: "8px", borderBottom: "1px solid #fff" }}>
+              Informasi
+            </Text>
+            {Object.keys(informationData).map((item) => (
+              <Text key={item} style={{ marginTop: "4px" }}>
+                {`${item}: ${informationData[item as keyof typeof informationData]}`}
+              </Text>
+            ))}
+          </WrapImage>
+
+          <WrapImage style={{ flex: 1 }}>
+            <div>
+              <Text
+                style={{ marginTop: "8px", borderBottom: "1px solid #fff" }}
+              >
+                Statistic
+              </Text>
+              {Object.keys(statisticData).map((item) => (
+                <Text key={item} style={{ marginTop: "4px" }}>
+                  {`${item}: ${statisticData[item as keyof typeof statisticData]}`}
+                </Text>
+              ))}
+            </div>
+            <div>
+              <Text
+                style={{
+                  marginTop: "8px",
+                  borderBottom: "1px solid #fff",
+                  marginBottom: "4px"
+                }}
+              >
+                Synopsis
+              </Text>
+              <Text>{data?.data.synopsis}</Text>
+            </div>
+          </WrapImage>
+        </Content>
+      )}
+    </>
+  );
 };
 
 export default Detail;
---
+++ src/pages/detail/styles.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Topbar = styled.div`
+  background-color: ${({ theme }) => theme.color.background};
+  height: 90px;
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  padding: 0 32px;
+  position: sticky;
+  top: 0;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 40px;
+`;
+
+export const ButtonBack = styled.div`
+  color: ${({ theme }) => theme.color.smokeWhite};
+  font-size: 20px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+`;
+
+export const Content = styled.div`
+  padding: 32px;
+  display: flex;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  align-self: center;
+  margin-bottom: 8px;
+`;
+
+export const WrapImage = styled.div`
+  border: 1px solid ${({ theme }) => theme.color.smokeWhite};
+  border-radius: 4px;
+  padding: 4px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Text = styled.div`
+  font-size: 16px;
+  color: ${({ theme }) => theme.color.smokeWhite};
+  font-weight: 500;
+`;
